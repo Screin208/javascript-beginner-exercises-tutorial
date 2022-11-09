13c73f3208f0f73dbfb1f541869d0d40acbc26e4
Screin208 <snay208@gmail.com>
2022-11-09 20:22:12 +0000

    hasta el 22

--- exercises/19-Random_colors_loop/app.js
+++ exercises/19-Random_colors_loop/app.js
@@ -16,11 +16,14 @@ function getColor(colorNumber = 0) {
 }
 
 function getAllStudentColors() {
-
-	//your loop here
-	let exampleColor = getColor(1);
+		//your loop here
+		let exampleColor = getColor(1);
+		for (let i = 0; i < 10; i++) {
+		getColor(Math.floor(Math.random() * (4 - 1) + 1))
+		
+	}
 }
 
 //call the function below with the number of students in the class and print on the console
-getAllStudentColors();
+console.log(getAllStudentColors());
 
--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,3 +1,30 @@
+let letras1= "let it be, "
+let letras2="whisper words of wisdom, "
+let letras3="there will be an answer, "
+let letras4= "let it be"
+
+function sing(){
+    let letra = ''
+
+for(let i = 1 ; i <= 12 ; i++){  
+  if(i==5){
+    letra += letras2
+ 
+  } else if(i==11){
+    letra += letras3
+
+  }else if (i == 12){
+    letra += letras4
+
+}else{
+    letra += letras1
+
+}
+    
+}
+
+return letra
+}
 
 
 //Your code above ^^^
